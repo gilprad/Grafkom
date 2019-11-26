b36c9a1654f1b25b21dc5108567dd3e6bd2d9825
gilprad <54804835+gilprad@users.noreply.github.com>
2019-11-26 10:09:03 +0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/projekq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekq.dir/main.cpp.o"
+  "projekq"
+  "projekq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
